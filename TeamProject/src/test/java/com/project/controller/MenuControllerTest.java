44a652e8394d51b0f49d237c6f700af1147ca6b5
Min0422 <hello.git.github@gmail.com>
2023-10-19 14:57:40 +0900

    임시 커밋

---
+++ TeamProject/src/test/java/com/project/controller/MenuControllerTest.java
@@ -0,0 +1,47 @@
+package com.project.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {com.project.config.RootConfig.class,
+								 com.project.config.ServletConfig.class})
+@Log4j
+public class MenuControllerTest {
+
+	@Setter(onMethod_ = { @Autowired })
+	private WebApplicationContext ctx;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test //입력
+	public void TestRegister() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+																  .param("title", "테스트 새글 제목")
+																  .param("content", "테스트 새글 내용")
+																  .param("writer", "user00")
+																  )
+										.andReturn()
+										.getModelAndView()
+										.getViewName();
+		log.info(resultPage);
+	}
+}
