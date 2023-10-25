cfa5d8245c7650d607cf9612ee493ed4604b1ef9
Min0422 <hello.git.github@gmail.com>
2023-10-25 09:40:26 +0900

    homecontroller 수정

--- TeamProject/src/main/java/com/project/controller/HomeController.java
+++ TeamProject/src/main/java/com/project/controller/HomeController.java
@@ -1,12 +1,17 @@
 package com.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.project.service.RestService;
 
 @Controller
 public class HomeController {
+	private RestService service;
+	
 	@GetMapping("/")
-	public String home() {
-		return "home";
+	public void home(Model model) {
+		model.addAttribute("category", service.getCodeList());
 	}
 }
