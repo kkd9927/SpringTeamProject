6fd2337bf97bd5ea7a5c35152d007cd585628392
kkd9927 <hello.git.kkd9927@gmail.com>
2023-10-26 14:13:06 +0900

    컨피그 수정

--- TeamProject/src/main/java/com/project/config/ServletConfig.java
+++ TeamProject/src/main/java/com/project/config/ServletConfig.java
@@ -1,6 +1,11 @@
 package com.project.config;
 
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -24,5 +29,17 @@ public class ServletConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**")
 				.addResourceLocations("/resources/");
+	}
+	
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getResolver() throws IOException {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setMaxUploadSize(1024 * 1024 * 10);
+		resolver.setMaxUploadSizePerFile(1024 * 1024 * 2);
+		resolver.setMaxInMemorySize(1024 * 1024);
+		resolver.setUploadTempDir(new FileSystemResource("C:\\upload\\tmp"));
+		resolver.setDefaultEncoding("UTF-8");
+		
+		return resolver;
 	}
 }
